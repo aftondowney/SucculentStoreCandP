ac3cdcec19706a46e22bc4866036a32e8f8c5b58
Afton Downey <aaike001@gmail.com>
2016-05-16 16:23:59 -0700

    add colorbox-plugin

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -212,7 +212,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'chicks-and-pricks',
+      'username' => 'chicks_admin',
+      'password' => 'chicks',
+      'host' => '127.0.0.1',
+      'port' => '8889',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -244,7 +258,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'CkF4jfUGuRmWHV4nEZI5hI3OIwo_oUJRXFewx4gb7Nw';
 
 /**
  * Base URL (optional).
